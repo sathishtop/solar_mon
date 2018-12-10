b71b6d00adebb70da0d099d63104d7d2bcd93195
Sathish <sathishtop@gmail.com>
2018-12-10 10:59:52 +0530

    Using the cumulative data feed to compute the daily data

--- emoncms_updater.py
+++ emoncms_updater.py
@@ -6,14 +6,13 @@ import argparse
 
 
 HISTORY_URL = "http://localhost/emoncms/input/post.json?node=10&apikey=c8152987954de00d0b4e07e35c765679&json="
-ENERGY_FEED_URL = "http://localhost/emoncms/feed/fetch.json?ids=36,56,55,52,53"
 data = {}
-PV_OUTPUT_URL_TEMPLATE = "https://pvoutput.org/service/r2/addoutput.jsp?key=2e7416cd761ffc1770997a0b379c71757bd11364&sid=62846&d={}&g={}&e={}&io={}"
 
-def do_update(iY,eY,cY):
+def do_update(iY,eY,cY,gY):
     data["impYesterday"] = iY 
     data["expYesterday"] = eY
     data["consumedYesterday"] = cY 
+    data["generatedYesterday"] = gY 
     updated_url = HISTORY_URL + json.dumps(data)
     print updated_url
     r = requests.get(updated_url)
@@ -23,5 +22,6 @@ if __name__ == '__main__':
     parser.add_argument("--importedYesterday", "-iY", help="Energy Imported Yesterday ")
     parser.add_argument("--exportedYesterday", "-eY", help="Energy Exported Yesterday ")
     parser.add_argument("--consumedYesterday", "-cY", help="Energy Consumed Yesterday ")
+    parser.add_argument("--generatedYesterday", "-gY", help="Energy Consumed Yesterday ")
     args = parser.parse_args()
-    do_update(args.importedYesterday, args.exportedYesterday,args.consumedYesterday)
+    do_update(args.importedYesterday, args.exportedYesterday,args.consumedYesterday,args.generatedYesterday)
--- pvoutput.py
+++ pvoutput.py
@@ -5,7 +5,7 @@ import requests, time
 import emoncms_updater
 
 HISTORY_URL = "http://localhost/emoncms/input/post.json?node=10&apikey=c8152987954de00d0b4e07e35c765679&json="
-ENERGY_FEED_URL = "http://localhost/emoncms/feed/fetch.json?ids=36,56,55,52,53"
+ENERGY_FEED_URL = "http://localhost/emoncms/feed/fetch.json?ids=59,52,53,62,61,60"
 data = {}
 PV_OUTPUT_URL_TEMPLATE = "https://pvoutput.org/service/r2/addoutput.jsp?key=2e7416cd761ffc1770997a0b379c71757bd11364&sid=62846&d={}&g={}&e={}&io={}"
 
@@ -15,13 +15,17 @@ def update_pvoutput():
         request = requests.get(ENERGY_FEED_URL)
         energy_data = json.loads(request.text)
         print energy_data
-        energy_generated = int(energy_data[0] * 1000)
-        energy_exported = int(energy_data[1] * 1000)
-        energy_imported = int(energy_data[2] * 1000)
+
+
+        energy_generated = int((energy_data[0] - energy_data[3]) * 1000)
+        energy_exported  = int((energy_data[1] - energy_data[4]) * 1000)
+        energy_imported  = int((energy_data[2] - energy_data[5]) * 1000)
+       
        # energy_consumed = int(energy_data[3] * 1000)
-        iY = energy_data[3]
-        eY = energy_data[4]
-        cY = energy_data[2] + energy_data[0] - energy_data[1]
+        gY = energy_data[0]
+        iY = energy_data[1]
+        eY = energy_data[2]
+        cY = get_consumed_pwer(energy_generated,energy_imported,energy_exported)
 
        # temperature_max = int(energy_data[5])
         date = datetime.today().strftime('%Y%m%d')
@@ -34,8 +38,12 @@ def update_pvoutput():
             time.sleep(3)
         else:
             print "Successfully uploaded the data in pvoutput, now updating emoncms"
-            emoncms_updater.do_update(iY,eY,cY)
+            emoncms_updater.do_update(iY,eY,cY,gY)
             break
+
+
+def get_consumed_pwer(generated, imported, exported):
+    return imported+generated-exported
 
 if __name__ == '__main__':
     update_pvoutput()
